3342d2f69072e9a7e7e5c343f0e9a2799c82610f
nxm403 <51175655+nxm403@users.noreply.github.com>
2022-11-22 21:55:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bv8ixg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bv8ixg)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from 'react';
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.css';
+import ProjectList from './ProjectList';
+import FormInput from './components/FormInput';
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App;
